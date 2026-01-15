b1d155e6b25ac2112a3efc585af41f781300d3cb
hunafazaky <50792018+hunafazaky@users.noreply.github.com>
2026-01-15 23:21:42 +0700

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: () => import('../views/SkillsView.vue'),
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: () => import('../views/PortfolioView.vue'),
+    },
   ],
 })
 
