418c30d256c7e1cea7db1f04d7a8c84c05f1f84d
hunafazaky <hunafa.zsn@gmail.com>
2026-03-29 22:49:50 +0700

    basic style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 function Navbar() {
   return (
-    <nav className="bg-gray-500 m-2 p-2">
-      <p>This is Navbar</p>
+    <nav className="bg-gray-500 p-2 left-0 right-0 top-0 sticky z-10 flex items-center justify-start gap-2">
+      <div>Content</div>
+      <div>Gallery</div>
     </nav>
   )
 }
