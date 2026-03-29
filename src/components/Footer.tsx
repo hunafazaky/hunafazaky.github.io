62f3bbf1450bf2ac03f1613a1801fd33ced08f55
hunafazaky <hunafa.zsn@gmail.com>
2026-03-29 23:22:46 +0700

    basic style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
-    <nav>
+    <footer className="bg-gray-950 text-gray-50 text-sm p-2 flex items-start justify-start flex-col gap-2">
       <p>This is Footer</p>
-    </nav>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, commodi.</p>
+    </footer>
   )
 }
 
