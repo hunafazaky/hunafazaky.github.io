0250be87c798c49d0591202eb63ae7a4b5fe63b0
hunafazaky <hunafa.zsn@gmail.com>
2026-04-14 20:17:25 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 // import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Content from './components/Content'
-import Library from './components/Library'
+import Navbar from './components/blocks/Navbar'
+import Hero from './components/blocks/Hero'
+import Gallery from './components/blocks/Gallery'
 import Footer from './components/Footer'
+
+const portfolio = [
+    {
+      id: 1,
+      title: "Project 1",
+      description: "Description of project 1",
+      image: "https://picsum.photos/300/200?random=1"
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "Description of project 2",
+      image: "https://picsum.photos/300/200?random=2"
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      description: "Description of project 3",
+      image: "https://picsum.photos/300/200?random=3"
+    },
+  ]
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Content />
-      <Library />
+      <Gallery title="Portfolio" items={portfolio}/>
       <Footer />
     </div>
   )
