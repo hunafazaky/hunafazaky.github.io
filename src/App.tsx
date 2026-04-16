30406950f35d774ea842b66a9413ba72f4254cf2
hunafazaky <hunafa.zsn@gmail.com>
2026-04-16 11:35:34 +0700

    comment unused code

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,57 @@
 import Navbar from "./components/blocks/Navbar";
 import Hero from "./components/blocks/Hero";
 import Content from "./components/blocks/Content";
-import Gallery from "./components/blocks/Gallery";
-import Footer from "./components/Footer";
+// import Gallery from "./components/blocks/Gallery";
+// import Footer from "./components/Footer";
 
 import Text from "./components/ui/Text";
 import H4 from "./components/ui/H4";
 import H5 from "./components/ui/H5";
 
-const questLog = [
-  {
-    id: 1,
-    title: "Local Weather App",
-    description:
-      "Local Weather App is a web application that provides real-time weather information based on the user's location. It utilizes the OpenWeatherMap API to fetch current weather data, including temperature, humidity, wind speed, and weather conditions. The app features a user-friendly interface with responsive design, allowing users to easily access weather information on various devices. Users can also search for weather conditions in different cities around the world. The Local Weather App is built using HTML, CSS, and JavaScript, making it a lightweight and efficient tool for staying informed about the weather.",
-    image: "https://picsum.photos/300/200?random=1",
-    ref: "https://hunafazaky.github.io/local-weather-app",
-  },
-  {
-    id: 2,
-    title: "Project 2",
-    description: "Description of project 2",
-    image: "https://picsum.photos/300/200?random=2",
-    ref: "/",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    description: "Description of project 3",
-    image: "https://picsum.photos/300/200?random=3",
-    ref: "/",
-  },
-];
-const achievements = [
-  {
-    id: 1,
-    title: "Achievements 1",
-    description: "Description of Achievements 1",
-    image: "https://picsum.photos/300/200?random=4",
-  },
-  {
-    id: 2,
-    title: "Achievements 2",
-    description: "Description of Achievements 2",
-    image: "https://picsum.photos/300/200?random=5",
-  },
-  {
-    id: 3,
-    title: "Achievements 3",
-    description: "Description of Achievements 3",
-    image: "https://picsum.photos/300/200?random=6",
-  },
-];
+// const questLog = [
+//   {
+//     id: 1,
+//     title: "Local Weather App",
+//     description:
+//       "Local Weather App is a web application that provides real-time weather information based on the user's location. It utilizes the OpenWeatherMap API to fetch current weather data, including temperature, humidity, wind speed, and weather conditions. The app features a user-friendly interface with responsive design, allowing users to easily access weather information on various devices. Users can also search for weather conditions in different cities around the world. The Local Weather App is built using HTML, CSS, and JavaScript, making it a lightweight and efficient tool for staying informed about the weather.",
+//     image: "https://picsum.photos/300/200?random=1",
+//     ref: "https://hunafazaky.github.io/local-weather-app",
+//   },
+//   {
+//     id: 2,
+//     title: "Project 2",
+//     description: "Description of project 2",
+//     image: "https://picsum.photos/300/200?random=2",
+//     ref: "/",
+//   },
+//   {
+//     id: 3,
+//     title: "Project 3",
+//     description: "Description of project 3",
+//     image: "https://picsum.photos/300/200?random=3",
+//     ref: "/",
+//   },
+// ];
+// const achievements = [
+//   {
+//     id: 1,
+//     title: "Achievements 1",
+//     description: "Description of Achievements 1",
+//     image: "https://picsum.photos/300/200?random=4",
+//   },
+//   {
+//     id: 2,
+//     title: "Achievements 2",
+//     description: "Description of Achievements 2",
+//     image: "https://picsum.photos/300/200?random=5",
+//   },
+//   {
+//     id: 3,
+//     title: "Achievements 3",
+//     description: "Description of Achievements 3",
+//     image: "https://picsum.photos/300/200?random=6",
+//   },
+// ];
 
 function App() {
   return (
